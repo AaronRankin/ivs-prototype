a9a03e9e27726ee60f9f9172394ca9643024ad92
AaronRankin <62940304+AaronRankin@users.noreply.github.com>
2020-05-11 08:41:49 +0100

    js fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -46,14 +46,6 @@ Layout.propTypes = {
 
 export default Layout
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (10 > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.transition = "all 300ms"
-    document.getElementById("header").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
-};
+
+
+
